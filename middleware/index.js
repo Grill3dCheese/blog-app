072769f0c2ab516d2854bfb9aa154a3d060b4055
Keith McKenna <keithmckenna@gmail.com>
2021-01-26 20:41:35 -0600

    update grammar for error messaging

--- middleware/index.js
+++ middleware/index.js
@@ -5,69 +5,74 @@ var User = require("../models/user");
 // all the middleware goes here
 var middlewareObj = {};
 
-middlewareObj.checkPostOwnership = function(req, res, next){
-   if(req.isAuthenticated()){
-        Post.findById(req.params.id, function(err, foundPost){
-        if(err || !foundPost){
-            console.log(err);
-            req.flash("error", "Sorry, we're not able to locate that entry. Please try again.");
-            res.redirect("/blog");
+middlewareObj.checkPostOwnership = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    Post.findById(req.params.id, function (err, foundPost) {
+      if (err || !foundPost) {
+        console.log(err);
+        req.flash(
+          "error",
+          "Sorry, we're not able to locate that entry. Please try again."
+        );
+        res.redirect("/blog");
+      } else {
+        //does user own the post?
+        if (foundPost.author.id.equals(req.user._id) || req.user.isAdmin) {
+          req.post = foundPost;
+          next();
         } else {
-            //does user own the post?
-            if(foundPost.author.id.equals(req.user._id) || req.user.isAdmin){
-                req.post = foundPost;
-                next();
-            } else {
-                req.flash("error", "You don't have permission to do that!");
-                res.redirect("/blog");
-            }
+          req.flash("error", "You don't have permission to do that!");
+          res.redirect("/blog");
         }
+      }
     });
-    } else {
-       req.flash("error", "You need to be logged in to perform that action.");
-       res.redirect("/login");
-    } 
-};
-
-middlewareObj.checkCommentOwnership = function(req, res, next){
-   if(req.isAuthenticated()){
-        Comment.findById(req.params.comment_id, function(err, foundComment){
-        if(err || !foundComment){
-            console.log(err);
-            req.flash("error", "Comment not found!");
-            res.redirect("back");
-        } else {
-            //does user own the comment?
-            if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
-                req.comment = foundComment;
-                next();
-            } else {
-                req.flash("error", "Apologies, you don't have permission to do that.");
-                res.redirect("back");
-            }
-        }
-    });
-    } else {
-        req.flash("error", "You need to be logged in to do that");
-        res.redirect("back");
-    } 
-};
-
-middlewareObj.isLoggedIn = function(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    req.flash("error", "You will need to be logged in to do that.");
+  } else {
+    req.flash("error", "You need to be logged in to perform that action.");
     res.redirect("/login");
+  }
 };
 
-middlewareObj.isUserAdmin = function(req, res, next) {
-	if(req.isAuthenticated() && req.user.isAdmin){
-		return next();
-	}
-    req.flash("error", "Apologies, you don't have permission to do that.");
-    res.redirect("/blog");
+middlewareObj.checkCommentOwnership = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+      if (err || !foundComment) {
+        console.log(err);
+        req.flash("error", "Comment not found!");
+        res.redirect("back");
+      } else {
+        //does user own the comment?
+        if (foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
+          req.comment = foundComment;
+          next();
+        } else {
+          req.flash(
+            "error",
+            "Apologies, you don't have permission to do that."
+          );
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    req.flash("error", "You need to be logged in to do that");
+    res.redirect("back");
+  }
 };
 
+middlewareObj.isLoggedIn = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.flash("error", "You need to be logged in to do that.");
+  res.redirect("/login");
+};
+
+middlewareObj.isUserAdmin = function (req, res, next) {
+  if (req.isAuthenticated() && req.user.isAdmin) {
+    return next();
+  }
+  req.flash("error", "Apologies, you don't have permission to do that.");
+  res.redirect("/blog");
+};
 
 module.exports = middlewareObj;
